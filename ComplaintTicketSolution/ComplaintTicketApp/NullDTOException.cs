78d16ddf50a60a89c405fd4265aabb157fee45c8
Purvika-22 <116261129+Purvika-22@users.noreply.github.com>
2023-11-22 23:50:02 +0530

    Add files via upload

---
+++ ComplaintTicketSolution/ComplaintTicketApp/NullDTOException.cs
@@ -0,0 +1,16 @@
+﻿using ComplaintTicketApp.Models;
+using System.Runtime.Serialization;
+
+namespace ComplaintTicketApplication.Exceptions
+{
+    [Serializable]
+    internal class NullDTOException : Exception
+    {
+        string message;
+        public NullDTOException()
+        {
+            message = "OrganizationDTO cannot be null.";
+        }
+        public override string Message => message;
+    }
+}
---
+++ ComplaintTicketSolution/ComplaintTicketApp/PriorityUpdateException.cs
@@ -0,0 +1,12 @@
+﻿namespace ComplaintTicketApplication.Exceptions
+{
+    public class PriorityUpdateException : Exception
+    {
+        string message;
+        public PriorityUpdateException()
+        {
+            message = "unable to update the priority , 500, \"Internal server error\" ";
+        }
+        public override string Message => message;
+    }
+}
