245f61193d5dcc0443b3f59fc12717b396fc8a0a
Praveena Vallela <praveenavallela599@gmail.com>
2023-11-23 18:54:57 +0530

    myCommit

--- ComplaintTicketSolution/ComplaintTicketApp/Interfaces/IPriorityService.cs
+++ ComplaintTicketSolution/ComplaintTicketApp/Interfaces/IPriorityService.cs
@@ -1,6 +1,25 @@
-﻿namespace ComplaintTicketApp.Interfaces
+﻿using System.Collections.Generic;
+using ComplaintTicketApp.Models;
+
+namespace ComplaintTicketApp.Interfaces
 {
     public interface IPriorityService
     {
+        // Get a priority by its ID
+        Priority GetPriorityById(int priorityId);
+
+        // Get all priority levels
+        IEnumerable<Priority> GetAllPriorities();
+
+        // Add a new priority
+        Priority AddPriority(Priority priority);
+
+        // Update an existing priority
+        Priority UpdatePriority(Priority priority);
+
+        // Remove a priority by its ID
+        Priority RemovePriority(int priorityId);
+
+        // Other methods as needed
     }
 }
--- ComplaintTicketSolution/ComplaintTicketApp/Services/PriorityService.cs
+++ ComplaintTicketSolution/ComplaintTicketApp/Services/PriorityService.cs
@@ -1,6 +1,44 @@
-﻿namespace ComplaintTicketApp.Services
+﻿using System.Collections.Generic;
+using ComplaintTicketApp.Interfaces;
+using ComplaintTicketApp.Models;
+using ComplaintTicketApp.Repositories;
+
+namespace ComplaintTicketApp.Services
 {
-    public class PriorityService
+    public class PriorityService : IPriorityService
     {
+        private readonly IRepository<int, Priority> _priorityRepository;
+
+        public PriorityService(IRepository<int, Priority> priorityRepository)
+        {
+            _priorityRepository = priorityRepository;
+        }
+
+        public Priority GetPriorityById(int priorityId)
+        {
+            return _priorityRepository.GetById(priorityId);
+        }
+
+        public IEnumerable<Priority> GetAllPriorities()
+        {
+            return _priorityRepository.GetAll();
+        }
+
+        public Priority AddPriority(Priority priority)
+        {
+            return _priorityRepository.Add(priority);
+        }
+
+        public Priority UpdatePriority(Priority priority)
+        {
+            return _priorityRepository.Update(priority);
+        }
+
+        public Priority RemovePriority(int priorityId)
+        {
+            return _priorityRepository.Delete(priorityId);
+        }
+
+        // You can add more methods as needed for specific business logic or operations
     }
 }
